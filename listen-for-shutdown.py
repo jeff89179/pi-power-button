b2fc74edbc1041148fa474d0f443036e573b7c90
Jeff <jeff89179@gmail.com>
2022-04-19 12:16:19 -0500

    Update listen-for-shutdown.py

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -10,4 +10,6 @@ GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.wait_for_edge(3, GPIO.FALLING)
 
 #subprocess.call(['shutdown', '-h', 'now'], shell=False)
-subprocess.call(['reboot'], shell=False)
+subprocess.call(['pkill', '--oldest', 'chromium'], shell=False)
+subprocess.call(['sleep', '3'], shell=False)
+subprocess.call(['reboot', '-h', 'now'], shell=False)
